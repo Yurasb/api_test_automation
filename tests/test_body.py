08b73bd9c1d7a7c5e779b37438a96b378efa7335
Yury Kuptsou <yurasb@tut.by>
2017-06-24 12:47:31 +0300

    1. fixed test names for headers and body 2. headers tests added

--- tests/test_body.py
+++ tests/test_body.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 
-def test_status_code(body, case):
+def test_body(body, case):
     assert body == case.expected_body
--- tests/test_headers.py
+++ tests/test_headers.py
@@ -1,5 +1,55 @@
 # -*- coding: utf-8 -*-
 
 
-def test_status_code(headers, case):
-    assert headers == case.expected_headers
+def test_id(headers):
+    assert 'X-GitHub-Request-Id' in headers
+
+
+def test_date(headers):
+    assert 'Date' in headers
+
+
+def test_expires(headers):
+    assert 'Expires' in headers
+
+
+def test_content_encoding(headers, case):
+    assert (
+        headers['Content-Encoding'] ==
+        case.expected_headers['Content-Encoding']
+    )
+
+
+def test_cache_control(headers, case):
+    assert (
+        headers['Cache-Control'] ==
+        case.expected_headers['Cache-Control']
+    )
+
+
+def test_server(headers, case):
+    assert (
+        headers['Server'] ==
+        case.expected_headers['Server']
+    )
+
+
+def test_content_type(headers, case):
+    assert (
+        headers['Content-Type'] ==
+        case.expected_headers['Content-Type']
+    )
+
+
+def test_access_control(headers, case):
+    assert (
+        headers['Access-Control-Allow-Origin'] ==
+        case.expected_headers['Access-Control-Allow-Origin']
+    )
+
+
+def test_transfer_encoding(headers, case):
+    assert (
+        headers['Transfer-Encoding'] ==
+        case.expected_headers['Transfer-Encoding']
+    )
